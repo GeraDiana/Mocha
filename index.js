b567bfa0dcefc58bc927de6ed305493a616da113
Diana Galushkina <dianna.gera@gmail.com>
2020-01-11 00:09:02 -0500

    fixed

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function sum(a,b){
-    return a + b;
+  return a + b;
 }
 function mult(a,b){
-    return a*b;
+  return a*b;
 }
 
-module.exports = {sum, mult};
+function isAdult(age){
+  if (age >= 21) return true;
+  if (age < 0) return 'No valid age ';
+  else return false;
+}
+
+module.exports = {sum, mult, isAdult};
+
+
+
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,20 @@
 const assert = require('assert');
 const {expect} = require('chai');
-const {sum, mult, power, concat, no, reverse} = require('../index.js');
+const {sum,isAdult, mult, power, concat, no, reverse} = require('../index.js');
 
 describe('function sum', () => {
-    it('sum 2 and 3 should equal 5', () => {
-        assert.equal(sum(2, 3), 5);
-    });
-    expect(sum (2, 4)).to.eq(6)
+  it('sum 2 and 3 should equal 5', () => {
+    assert.equal(sum(2, 3), 5);
+  });
+  expect(sum (2, 4)).to.eq(6);
 
-    it('sum 2 and 3 should not equal 5', () => {
-        assert.notEqual(sum(2, 3), 0)
-    });
-})
+  it('sum 2 and 3 should not equal 5', () => {
+    assert.notEqual(sum(2, 3), 0);
+  });
+});
 
+describe('function isAdult', () => {
+  it('function isAdult should be true',() => {
+    assert.equal(isAdult(21), true);
+  });
+});
